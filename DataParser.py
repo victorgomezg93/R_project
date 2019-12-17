c180f1b90c02bfde07691ede04c0e2bc81771f38
Marcportavella <marc.portavella@gmail.com>
2019-12-17 19:06:38 +0100

    Added country and date to dataframe generation

--- DataParser.py
+++ DataParser.py
@@ -2,6 +2,7 @@ import os
 import glob
 import pandas
 import geoip2.database as db
+import datetime
 
 # source = r"D:\Master_Cybersecurity_management\05-Data_driven_security\blocklist-ipsets-master"
 # dest = r"D:\Master_Cybersecurity_management\05-Data_driven_security\Practica\data.csv"
@@ -9,14 +10,15 @@ import geoip2.database as db
 def getdataframe(source,dest = ""):
 	files = glob.glob(os.path.join(source,"*.ipset"))
 
-	dftotal = pandas.DataFrame(columns=["IP","Category","Maintainer","Country"])
+	dftotal = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
 	reader = db.Reader(r"./GeoLite2-Country_20191210/GeoLite2-Country.mmdb")
 	for f in files:
 		ips = []
 		countrylist = []
 		cat = ""
 		main = ""
-		df = pandas.DataFrame(columns=["IP","Category","Maintainer","Country"])
+		date = ""
+		df = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
 
 		aux = open(f,"r")
 		content = aux.readlines()
@@ -29,10 +31,17 @@ def getdataframe(source,dest = ""):
 				main = str(l.split(":")[-1]).strip().replace("/","")
 			elif "#" not in l:
 				ips.append(str(l).strip())
+			elif "This File Date" in l:
+				date = str(l.split(" : ")[-1]).strip().replace("  "," ")
+				try:
+					date = str(datetime.datetime.strptime(date, "%a %b %d %H:%M:%S UTC %Y"))
+				except:
+					print(str(l))
 
 		df["IP"] = ips
 		df["Category"] = [cat]*len(ips)
 		df["Maintainer"] = [main]*len(ips)
+		df["Date"] = [date]*len(ips)
 
 		for ip in df.IP.values:
 			try:
--- Run.py
+++ Run.py
@@ -1,14 +1,14 @@
 import DataParser as dp
 import pandas
-# df es un dataframe con la informacion parseada: ip, categoria y fuente
+# df es un dataframe con la informacion parseada: ip, categoria, fuente, pais y fecha
 # df = dp.getdataframe(r"./blocklist-ipsets-master")
-# df.to_pickle(r"./brutedataframe.pkl")
-df = pandas.read_pickle(r"./brutedataframe.pkl")
+# df.to_pickle(r"./brutedataframe_country_date.pkl")
+df = pandas.read_pickle(r"./brutedataframe_country_date.pkl")
 # df1 es un dataframe quitando los duplicados teniendo en cuenta la combinacion ip+categoria
 # tiene una columna que cuenta las ocurrencias de los duplicados eliminados
-# df1 = dp.dropduplicates(df)
-# df1.to_pickle(r"./noduplicatesdataframe.pkl")
-df1 = pandas.read_pickle(r"./noduplicatesdataframe.pkl")
+df1 = dp.dropduplicates(df)
+df1.to_pickle(r"./noduplicatesdataframe_country_date.pkl")
+# df1 = pandas.read_pickle(r"./noduplicatesdataframe_country_date.pkl")
 # df2 es un datafreme que tiene como indice las ips y como columnas los distintos tipos de categoria
 # df2 = dp.spread(df1)
 # df2.to_pickle(r"./categorydataframe.pkl")
