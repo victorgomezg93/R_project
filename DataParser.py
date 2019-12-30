3764c378d8da2c9d5ceb14c207951ffcbab08e3d
Marc Portavella <marc_portavella@serimagmedia.com>
2019-12-30 10:14:27 +0100

    Markdown update + html document

--- DataParser.py
+++ DataParser.py
@@ -8,111 +8,113 @@ import datetime
 # dest = r"D:\Master_Cybersecurity_management\05-Data_driven_security\Practica\data.csv"
 
 def getdataframe(source,dest = ""):
-	files = glob.glob(os.path.join(source,"*.ipset"))
+    files = glob.glob(os.path.join(source,"*.ipset"))
 
-	dftotal = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
-	reader = db.Reader(r"./GeoLite2-Country_20191210/GeoLite2-Country.mmdb")
-	for f in files:
-		ips = []
-		countrylist = []
-		cat = ""
-		main = ""
-		date = ""
-		df = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
+    dftotal = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
+    reader = db.Reader(r"./GeoLite2-Country_20191210/GeoLite2-Country.mmdb")
+    for f in files:
+        ips = []
+        countrylist = []
+        cat = ""
+        main = ""
+        date = ""
+        df = pandas.DataFrame(columns=["IP","Category","Maintainer","Country","Date"])
 
-		aux = open(f,"r")
-		content = aux.readlines()
-		aux.close()
+        aux = open(f,"r")
+        content = aux.readlines()
+        aux.close()
 
-		for l in  content:
-			if "Category" in l:
-				cat = str(l.split(":")[-1]).strip()
-			elif "Maintainer" in l:
-				main = str(l.split(":")[-1]).strip().replace("/","")
-			elif "#" not in l:
-				ips.append(str(l).strip())
-			elif "This File Date" in l:
-				date = str(l.split(" : ")[-1]).strip().replace("  "," ")
-				try:
-					date = str(datetime.datetime.strptime(date, "%a %b %d %H:%M:%S UTC %Y"))
-				except:
-					print(str(l))
+        for l in  content:
+            if "Category" in l:
+                cat = str(l.split(":")[-1]).strip()
+            elif "Maintainer" in l:
+                main = str(l.split(":")[-1]).strip().replace("/","")
+            elif "#" not in l:
+                ips.append(str(l).strip())
+            elif "This File Date" in l:
+                date = str(l.split(" : ")[-1]).strip().replace("  "," ")
+                try:
+                    date = str(datetime.datetime.strptime(date, "%a %b %d %H:%M:%S UTC %Y"))
+                except:
+                    print(str(l))
 
-		df["IP"] = ips
-		df["Category"] = [cat]*len(ips)
-		df["Maintainer"] = [main]*len(ips)
-		df["Date"] = [date]*len(ips)
+        df["IP"] = ips
+        df["Category"] = [cat]*len(ips)
+        df["Maintainer"] = [main]*len(ips)
+        df["Date"] = [date]*len(ips)
 
-		for ip in df.IP.values:
-			try:
-				c = reader.country(ip).country.names["en"]
-			except:
-				c = ''
-			countrylist.append(c)
+        for ip in df.IP.values:
+            try:
+                c = reader.country(ip).country.names["en"]
+            except:
+                c = ''
+            countrylist.append(c)
 
-		df["Country"] = countrylist
-		
-		dftotal = dftotal.append(df, ignore_index = True)
+        df["Country"] = countrylist
+        
+        dftotal = dftotal.append(df, ignore_index = True)
+    if dest != "" and not os.path.exists(dest):
+        dftotal.drop(columns=dftotal.columns[0]).to_csv(dest)
 
-	return dftotal
-	
-def dropduplicates(df):
-	aux = df
-	aux["Occurrence"] = df.groupby(["IP","Category"]).cumcount() + 1
-	return aux.drop_duplicates(subset = ["IP","Category"], keep = "last")
+    return dftotal.drop(columns=dftotal.columns[0])
+    
+def dropduplicates(df,groupcolumns):
+    aux = df
+    aux["Occurrence"] = df.groupby(groupcolumns).cumcount() + 1
+    return aux.drop_duplicates(subset = groupcolumns, keep = "last")
 
 def dropduplicates2(df):
-	aux = df
-	aux["Occurrence"] = df.groupby("IP").cumcount() + 1
-	return aux.drop_duplicates(subset = "IP", keep = "last")
+    aux = df
+    aux["Occurrence"] = df.groupby("IP").cumcount() + 1
+    return aux.drop_duplicates(subset = "IP", keep = "last")
 
 def dropduplicates3(df):
-	aux = df
-	aux["Occurrence"] = df.groupby(["Category","Country"]).cumcount() + 1
-	return aux.drop_duplicates(subset = ["Category", "Country"], keep = "last")
+    aux = df
+    aux["Occurrence"] = df.groupby(["Category","Country"]).cumcount() + 1
+    return aux.drop_duplicates(subset = ["Category", "Country"], keep = "last")
 
 def spread(df):
-	return df.pivot(index = "IP", columns = "Category", values = "Occurrence")
+    return df.pivot(index = "IP", columns = "Category", values = "Occurrence")
 
 def spread2(df):
-	return df.pivot(index = "IP", columns = "Country", values = "Occurrence")
+    return df.pivot(index = "IP", columns = "Country", values = "Occurrence")
 
 def spread3(df):
-	return df.pivot(index = "Country", columns = "Category", values = "Occurrence")
+    return df.pivot(index = "Country", columns = "Category", values = "Occurrence")
 
 def correlation(df3,df4):
-	normalized = []
-	country = []
-	for c in df4.columns[2:]:
-		try:
-			a = int(df4[c])
-			b = int(df3.loc[df3["Country Name"] == c]["2018"])
-			normalized.append((a/b)*1000)
-			country.append(c)
-		except:
-			pass
-	aux = pandas.DataFrame(columns=["Country","Risk"])
-	aux["Country"] = country
-	aux["Risk"] = normalized
+    normalized = []
+    country = []
+    for c in df4.columns[2:]:
+        try:
+            a = int(df4[c])
+            b = int(df3.loc[df3["Country Name"] == c]["2018"])
+            normalized.append((a/b)*1000)
+            country.append(c)
+        except:
+            pass
+    aux = pandas.DataFrame(columns=["Country","Risk"])
+    aux["Country"] = country
+    aux["Risk"] = normalized
 
-	return aux
+    return aux
 
 def correlation2(df):
-	it = df.iterrows()
-	try:
-		next(it)
-		country = []
-		category = []
-		for i in range(len(df.index)):
-			aux = next(it)
-			country.append(aux[0])
-			a1 = aux[1].keys().to_list()
-			a2 = list(aux[1].values)
-			c = a1[a2.index(max(a2))]
-			category.append(c)
-	except:
-		pass
-	dfret = pandas.DataFrame(columns=["Country","Category"])
-	dfret["Country"] = country
-	dfret["Category"] = category
-	return dfret
+    it = df.iterrows()
+    try:
+        next(it)
+        country = []
+        category = []
+        for i in range(len(df.index)):
+            aux = next(it)
+            country.append(aux[0])
+            a1 = aux[1].keys().to_list()
+            a2 = list(aux[1].values)
+            c = a1[a2.index(max(a2))]
+            category.append(c)
+    except:
+        pass
+    dfret = pandas.DataFrame(columns=["Country","Category"])
+    dfret["Country"] = country
+    dfret["Category"] = category
+    return dfret
--- Run.py
+++ Run.py
@@ -5,24 +5,23 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import time
 # df es un dataframe con la informacion parseada: ip, categoria, fuente, pais y fecha
-start = time.time()
-df = dp.getdataframe(r"./blocklist-ipsets-master")
-df.to_csv(r"./brutedataframe_country_date.csv")
-print("Parseado+pais: ", time.time()-start)
-# df = pandas.read_csv(r".\brutedataframe_country_date.csv")
+# start = time.time()
+# df = dp.getdataframe(r"./blocklist-ipsets-master", r"./brutedataframe_country_date.csv")
+# print("Parseado+pais: ", time.time()-start)
+df = pandas.read_csv(r".\brutedataframe_country_date.csv")
 # df1 es un dataframe quitando los duplicados teniendo en cuenta la combinacion ip+categoria
 # tiene una columna que cuenta las ocurrencias de los duplicados eliminados
-start = time.time()
-df1 = dp.dropduplicates(df)
-df1.to_csv(r"./noduplicatesdataframe_country_date.csv")
-print("dropduplicates: ", time.time()-start)
-# df1 = pandas.read_csv(r".\noduplicatesdataframe_country_date.csv")
+# start = time.time()
+# df1 = dp.dropduplicates(df,["IP","Category"])
+# df1.to_csv(r"./noduplicatesdataframe_country_date.csv")
+# print("dropduplicates: ", time.time()-start)
+df1 = pandas.read_csv(r".\noduplicatesdataframe_country_date.csv")
 # df2 es un datafreme que tiene como indice las ips y como columnas los distintos tipos de categoria
-start = time.time()
-df2 = dp.spread(df1)
-df2.to_csv(r"./categorydataframe.csv")
-print("spread: ", time.time()-start)
-# df2 = pandas.read_csv(r".\categorydataframe.csv")
+# start = time.time()
+# df2 = dp.spread(df1)
+# df2.to_csv(r"./categorydataframe.csv")
+# print("spread: ", time.time()-start)
+df2 = pandas.read_csv(r".\categorydataframe.csv")
 #df3 es un dataframe que contiene informacion demografica por pais
 df3 = pandas.read_csv(r".\PopulationData\API_SP.POP.TOTL_DS2_en_csv_v2_566132.csv")
 
@@ -30,20 +29,20 @@ df3 = pandas.read_csv(r".\PopulationData\API_SP.POP.TOTL_DS2_en_csv_v2_566132.cs
 # df4 = dp.spread2(df4)
 # df4 = df4.agg(["sum"])
 # df4.to_csv(r".\ipsbycountry.csv")
-start = time.time()
+# start = time.time()
 df4 = pandas.read_csv(r".\ipsbycountry.csv")
 df5 = dp.correlation(df3,df4)
 df5.sort_values(by="Risk")
-df5.to_csv(r".\CountryRisk.csv")
-print("CountryRisk: ", time.time()-start)
+# df5.to_csv(r".\CountryRisk.csv")
+# print("CountryRisk: ", time.time()-start)
 
-start = time.time()
+# start = time.time()
 dfcat = df # df[df.Category != "abuse"]
 df6 = dp.dropduplicates3(dfcat)
 df6 = dp.spread3(df6)
 df6 = dp.correlation2(df6)
-df6.to_csv(r".\CategorybyCountry.csv")
-print("CategorybyCountry: ", time.time()-start)
+# df6.to_csv(r".\CategorybyCountry.csv")
+# print("CategorybyCountry: ", time.time()-start)
 
 
 #bar plot that shows Category weights for 1 day of malicious IPS
